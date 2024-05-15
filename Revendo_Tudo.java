82cf2cb0c0349cab44492f9556499c11a552c12f
LABC10 - Note 10 <thawanferreira628@gmail.com>
2024-05-15 17:01:56 -0300

    testei variaveis

---
+++ Revendo_Tudo.java
@@ -0,0 +1,25 @@
+
+public class Revendo_Tudo {
+
+	public static void main(String[] args) {
+		
+		// Declarando as variaveis
+		int valor_Inicial = 7;
+		float valorFracionado = 1.8f;
+		double valor_Decimal = 37.89;
+		boolean pergunta = true;
+		String palavra = "Dia 9";
+		
+		// Criando uma constante 
+		final int NUMERO = 9;
+		
+		System.out.println("Valor: "+valor_Inicial);
+		System.out.println("Valor: "+valorFracionado);
+		System.out.println("Valor: "+valor_Decimal);
+		System.out.println("Pergunta: "+pergunta);
+		System.out.println("Uma palavra: "+palavra);
+		System.out.println("Numero: "+Math.PI);
+
+	}
+
+}
